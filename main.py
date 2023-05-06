20dc19bb58786fe6d72b00056fbd5e0112e718f0
hanif <hanifdiyer@gmail.com>
2023-05-06 14:17:06 +0200

    adding type hinting

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import argparse
 import math
 import random
 import numpy as np
+import numpy.typing as npt
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 
 
 class Line:
-    def __init__(self, m, c, inlier_indices=[]):
+    def __init__(self, m: float, c: float, inlier_indices: list = []):
         self.m = m
         self.c = c
         self.inlier_indices = inlier_indices
@@ -26,38 +27,13 @@ class Line:
         return len(self.inlier_indices)
 
 
-def fit_line(points):
-    n = len(points)
-
-    # Calculate the mean of x and y
-    x_mean = sum([point[0] for point in points]) / n
-    y_mean = sum([point[1] for point in points]) / n
-
-    # Calculate the slope and y-intercept of the linelower
-    numerator = sum(
-        [(points[i][0] - x_mean) * (points[i][1] - y_mean) for i in range(n)]
-    )
-    denominator = sum([(points[i][0] - x_mean) ** 2 for i in range(n)])
-    m = numerator / denominator
-    c = y_mean - m * x_mean
-
-    # Return the slope and y-intercept
-    return m, c
-
-
-def distance_to_line(point, line):
-    # Unpack the point coordinates
-    x, y = point
-
-    # Calculate the distance from the point to the line
-    distance = abs(line.m * x - y + line.c) / math.sqrt(line.m**2 + 1)
-
-    return distance
-
-
 def sequential_ransac_multi_line_detection(
-    data, threshold, min_points, max_iterations, max_lines
-):
+    data: npt.NDArray[np.float64],
+    threshold: float,
+    min_points: int,
+    max_iterations: int,
+    max_lines: int,
+) -> npt.NDArray[np.float64]:
 
     best_lines = []
     remaining_data = data
@@ -84,7 +60,7 @@ def sequential_ransac_multi_line_detection(
         inlier_indices = []
         # slope, intercept = best_line.get_line_explicit()
         for j, (x, y) in enumerate(remaining_data):
-            if distance_to_line((x, y), best_line) < threshold:
+            if calc_dist_to_line((x, y), best_line) < threshold:
                 inlier_indices.append(j)
 
         remaining_data = [
@@ -92,20 +68,26 @@ def sequential_ransac_multi_line_detection(
             for j in range(len(remaining_data))
             if j not in inlier_indices
         ]
+        remaining_data = np.array(remaining_data)
 
         # second stopping condition
         if len(remaining_data) <= 5:
             break
 
-    return best_lines
+    return np.array(best_lines)
 
 
-def ransac_line_detection(data, threshold, min_points, max_iterations):
+def ransac_line_detection(
+    data: npt.NDArray[np.float64],
+    threshold: float,
+    min_points: int,
+    max_iterations: int,
+) -> Line:
 
     best_num_inliers = 0
     for i in range(max_iterations):
         # randomly select a subset of data points
-        sample = random.sample(data, min_points)
+        sample = data[np.random.choice(data.shape[0], 2, replace=False), :]
 
         # fit a line to the subset of data points
         x1, y1 = sample[0]
@@ -116,14 +98,14 @@ def ransac_line_detection(data, threshold, min_points, max_iterations):
             continue
         slope = (y2 - y1) / (x2 - x1)
         intercept = y1 - slope * x1
-        
-        fitted_line = Line(slope, intercept) 
+
+        fitted_line = Line(slope, intercept)
 
         # count the number of inliers (data points that are within the threshold distance of the line)
         curr_num_inliers = 0
         inliers = []
         for idx, (x, y) in enumerate(data):
-            if distance_to_line((x, y), fitted_line) < threshold:
+            if calc_dist_to_line((x, y), fitted_line) < threshold:
                 inliers.append((x, y))
                 curr_num_inliers += 1
 
@@ -135,7 +117,47 @@ def ransac_line_detection(data, threshold, min_points, max_iterations):
     return best_line_model
 
 
-def polar_to_cartesian(polar_points):
+def fit_line(points: list):
+    n = len(points)
+
+    # Calculate the mean of x and y
+    x_mean = sum([point[0] for point in points]) / n
+    y_mean = sum([point[1] for point in points]) / n
+
+    # Calculate the slope and y-intercept of the linelower
+    numerator = sum(
+        [(points[i][0] - x_mean) * (points[i][1] - y_mean) for i in range(n)]
+    )
+    denominator = sum([(points[i][0] - x_mean) ** 2 for i in range(n)])
+    m = numerator / denominator
+    c = y_mean - m * x_mean
+
+    # Return the slope and y-intercept
+    return m, c
+
+
+def calc_dist_to_line(point: tuple, line: Line) -> float:
+    # Unpack the point coordinates
+    x, y = point
+
+    # Calculate the distance from the point to the line
+    distance = abs(line.m * x - y + line.c) / math.sqrt(line.m**2 + 1)
+
+    return distance
+
+
+def calc_dist_to_point(point1: tuple, point2: tuple):
+
+    x1, y1 = point1
+    x2, y2 = point2
+    dx = x2 - x1
+    dy = y2 - y1
+    return math.sqrt(dx * dx + dy * dy)
+
+
+def polar_to_cartesian(
+    polar_points: npt.NDArray[np.float64],
+) -> npt.NDArray[np.float64]:
 
     cartesian_points = []
     for point in polar_points:
@@ -148,10 +170,10 @@ def polar_to_cartesian(polar_points):
 
         cartesian_points.append((x_cartesian, y_cartesian))
 
-    return cartesian_points
+    return np.array(cartesian_points)
 
 
-def load_points_file(filename):
+def load_points_file(filename: str):
 
     points = []
     with open(filename, "r") as f:
@@ -164,30 +186,22 @@ def load_points_file(filename):
     return points
 
 
-def acute_angle_between_lines(m1, m2):
-    theta = abs(math.atan(m2) - math.atan(m1))
+def calc_acute_angle(line1: Line, line2: Line):
+
+    theta = abs(math.atan(line2.m) - math.atan(line1.m))
     theta_degrees = math.degrees(theta)
     return theta_degrees if theta_degrees <= 90 else 180 - theta_degrees
 
 
-def find_intersection(m1, b1, m2, b2):
+def find_intersection(line1: Line, line2: Line):
     # Calculate the intersection point
-    x = (b2 - b1) / (m1 - m2)
-    y = m1 * x + b1
+    x = (line2.c - line1.c) / (line1.m - line2.m)
+    y = line1.m * x + line1.c
 
     return x, y
 
 
-def distance(point1, point2):
-
-    x1, y1 = point1
-    x2, y2 = point2
-    dx = x2 - x1
-    dy = y2 - y1
-    return math.sqrt(dx * dx + dy * dy)
-
-
-def find_connected_line_pair(detected_lines):
+def find_connected_line_pair(detected_lines: npt.NDArray[np.float64]):
 
     # draw circle
     best_intersection = None
@@ -199,37 +213,34 @@ def find_connected_line_pair(detected_lines):
             line2 = detected_lines[j]
 
             radius = 50
-            center = find_intersection(
-                line1.m,
-                line1.c,
-                line2.m,
-                line2.c,
-            )
+            center = find_intersection(line1, line2)
 
             inside_circle_cnt = 0
             # the inlier_idx is not actually an index
             for inlier_idx in line1.inlier_indices:
-                if distance(inlier_idx, center) < radius:
+                if calc_dist_to_point(inlier_idx, center) < radius:
                     inside_circle_cnt += 1
 
             for inlier_idx in line2.inlier_indices:
-                if distance(inlier_idx, center) < radius:
+                if calc_dist_to_point(inlier_idx, center) < radius:
                     inside_circle_cnt += 1
 
             print("inside circle count", inside_circle_cnt)
-            print("acute angle", acute_angle_between_lines(line1.m, line2.m))
+            print("acute angle", calc_acute_angle(line1, line2))
 
             if (
                 inside_circle_cnt > best_inside_circle_cnt
-                and acute_angle_between_lines(line1.m, line2.m) > 85
+                and calc_acute_angle(line1, line2) > 85
             ):
-                best_intersection = [detected_lines[i], detected_lines[j]]
+                best_intersection = (detected_lines[i], detected_lines[j])
                 best_inside_circle_cnt = inside_circle_cnt
 
     return best_intersection
 
 
-def visualize_lines(cartesian_points, best_intersection):
+def visualize_lines(
+    cartesian_points: npt.NDArray[np.float64], best_intersection: tuple
+):
 
     x_coords = [p[0] for p in cartesian_points]
     y_coords = [p[1] for p in cartesian_points]
@@ -250,7 +261,9 @@ def visualize_lines(cartesian_points, best_intersection):
         ax.plot(x, y)
 
 
-def visualize_points_polar(points_polar, lidar, SCAN_RANGE):
+def visualize_points_polar(
+    points_polar: npt.NDArray[np.float64], lidar: str, SCAN_RANGE: dict
+):
 
     thetas = [p[0] for p in points_polar]
     rhos = [p[1] for p in points_polar]
@@ -282,8 +295,8 @@ def main():
     args = parser.parse_args()
 
     SCAN_RANGE = {
-        "ANGLE_V": [130, 160],
-        "ANGLE_H": [170, 210],
+        "ANGLE_V": (130, 160),
+        "ANGLE_H": (170, 210),
         "DIST_V": 2000,
         "DIST_H": 2000,
     }
@@ -308,15 +321,17 @@ def main():
 
     points_filtered = points[mask]
 
-    # required minimal 2 data points to fit line
-    if points_filtered.size < 4:
+    # four data points are required to fit two lines
+    if points_filtered.size < 8:
         print(f"The number of input points are too small: {points_filtered.size}")
         return
 
     cartesian_points = polar_to_cartesian(points_filtered)
 
+    # number_int = 23
     detected_lines = sequential_ransac_multi_line_detection(
         cartesian_points,
+        # number_int,
         threshold=args.threshold,
         min_points=2,
         max_iterations=args.iter,
@@ -326,7 +341,7 @@ def main():
     # find the line pair denoting the two edges of the box
     best_line_pair = find_connected_line_pair(detected_lines)
 
-    # visualization 
+    # visualization
     visualize_lines(cartesian_points, best_line_pair)
     visualize_points_polar(points_filtered, args.lidar, SCAN_RANGE)
     plt.show(block=True)
